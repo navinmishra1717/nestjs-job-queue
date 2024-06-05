4ff99642705fdb2ec477cdefb9a80677384e6b3d
Navin Mishra <navinmishra1717@gmail.com>
2024-06-05 17:34:36 +0545

    setup nestjs bull for queue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,35 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { BullModule } from "@nestjs/bull";
+import { UsersModule } from "./users/users.module";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 
 @Module({
-  imports: [],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: "localhost",
+        port: 6379,
+      },
+    }),
+
+    BullModule.registerQueue({
+      name: "emailSending",
+    }),
+
+    UsersModule,
+
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
+      // plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      include: [UsersModule],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/common/services/email.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@nestjs/common";
+import { EmailSendData, IMail } from "../interface/email.interface";
+import { Queue } from "bull";
+import { InjectQueue } from "@nestjs/bull";
+import { EmailType } from "../enum/email-type.enum";
+
+@Injectable()
+export class EmailService {
+  constructor(
+    @InjectQueue("emailSending")
+    private readonly emailQueue: Queue,
+  ) {}
+  async sendEmail<T extends EmailType>(emailType: T, data: EmailSendData[T]) {
+    const job = await this.emailQueue.add(emailType, {
+      data,
+    });
+
+    console.log(`Job ${job.id} has been added to the queue.`);
+
+    return { jobId: job.id };
+  }
+}
